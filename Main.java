19d60b17efabe144a7dabccfa84e4350909f0572
andras <kandras@digikabel.hu>
2015-11-21 15:37:55 +0100

    contain test class

--- Main.java
+++ Main.java
@@ -12,12 +12,14 @@ public class Main {
 		Manuf_scope manuf_scope = new Manuf_scope();
 		Order order = new Order();
 		Product product = new Product();
+		Contain contain = new Contain();
 		
 		/**
 		 *  Clear DB
 		 */
 		user.delete(user);
 		role.delete(role);
+		contain.delete(contain);
 		order.delete(order);
 		manuf_scope.delete(manuf_scope);
 		product.delete(product);
@@ -61,6 +63,9 @@ public class Main {
 		user.set_role_id(null);
 		
 		user.update(user);
+		
+		user = new User();	
+		user.delete(user);
 		*/
 		
 		
@@ -93,6 +98,10 @@ public class Main {
 		role.set_name("master2");
 	
 		role.update(role);
+
+		role = new Role();
+		role.delete(role);
+
 		*/
 		
 		/**
@@ -126,6 +135,10 @@ public class Main {
 		manuf_scope.set_deadline("20160101");
 	
 		manuf_scope.update(manuf_scope);
+		
+		manuf_scope = new Manuf_scope();
+		manuf_scope.delete(manuf_scope);
+
 		*/
 		
 		
@@ -133,46 +146,52 @@ public class Main {
 		 *  Order class test
 		 */	
 		/*
-		manuf_scope.set_ms_id("manuf_scope10");
+		manuf_scope.set_ms_id("manuf_scope1");
 		manuf_scope.set_is_ordered(0);
 		manuf_scope.set_deadline("20151201");
 		manuf_scope.add(manuf_scope);
 
-		product.set_name("product10");
+		product.set_name("product1");
 		product.add(product);
 		
-		order.set_manuf_scope_id(manuf_scope.get(manuf_scope).get(0).get_id());
-		order.set_product_id(product.get(product).get(0).get_id());
-		order.set_number(10);
-		order.add(order);
-		
-		manuf_scope.set_ms_id("manuf_scope11");
+		manuf_scope.set_ms_id("manuf_scope2");
 		manuf_scope.set_is_ordered(0);
 		manuf_scope.set_deadline("20151201");
 		manuf_scope.add(manuf_scope);
 		
+		product.set_name("product2");
+		product.add(product);
+		
+		manuf_scope = new Manuf_scope();
+		product = new Product();
+		
 		order.set_manuf_scope_id(manuf_scope.get(manuf_scope).get(0).get_id());
 		order.set_product_id(product.get(product).get(0).get_id());
 		order.set_number(10);
 		order.add(order);
-
-		List<Order> ord_list = new ArrayList<Order>();
 		
-		ord_list = order.get(order);
+		order.set_manuf_scope_id(manuf_scope.get(manuf_scope).get(1).get_id());
+		order.set_product_id(product.get(product).get(1).get_id());
+		order.set_number(15);
+		order.add(order);
 		
-		System.out.println(ord_list.get(0));
+		System.out.println(order.get(order).get(0));
 		
 		order.delete(order);
-		
-		manuf_scope.set_ms_id("manuf_scope10");
-		manuf_scope.set_is_ordered(0);
-		manuf_scope.set_deadline("20151201");
-		
+				
 		order.set_manuf_scope_id(manuf_scope.get(manuf_scope).get(0).get_id());
 		order.set_product_id(product.get(product).get(0).get_id());
-		order.set_number(15);
+		order.set_number(20);
 		
 		order.update(order);
+		System.out.println(order.get(order).get(0));
+
+		order = new Order();
+		product = new Product();
+		contain = new Contain();
+		order.delete(order);
+		manuf_scope.delete(manuf_scope);
+		product.delete(product);
 		*/
 		
 		/**
@@ -204,15 +223,76 @@ public class Main {
 		product.set_name("big_axe");
 	
 		product.update(product);
+		
+		product = new Product();
+		product.delete(product);
 		*/
+		
+		/**
+		 *  Contain class test
+		 */	
+		/*
+		product.set_name("product1");
+		product.add(product);
+		
+		//add component
+
+		product.set_name("product2");
+		product.add(product);
+		
+		//add component
+		
+		manuf_scope = new Manuf_scope();
+		product = new Product();
+		
+		contain.set_product_id(product.get(product).get(0).get_id());
+		contain.set_component_id(1);//(component.get(component).get(0).get_id());
+		contain.set_number(10);
+		contain.add(contain);
+		
+		contain.set_product_id(product.get(product).get(1).get_id());
+		contain.set_component_id(1);//(component.get(component).get(1).get_id());
+		contain.set_number(15);
+		contain.add(contain);
+		
+		System.out.println(contain.get(contain).get(0));
+		
+		contain.delete(contain);
+				
+		contain.set_product_id(product.get(product).get(0).get_id());
+		contain.set_component_id(1);//(component.get(component).get(0).get_id());
+		contain.set_number(20);
+		
+		contain.update(contain);
+		System.out.println(contain.get(contain).get(0));
+
+		contain = new Contain();
+		product = new Product();
+		//component = new Contain();
+		contain.delete(contain);
+		product.delete(product);
+		//compoent.delete(product);
+		*/
+	
+		
 		
 		/**
 		 *  Clear DB
 		 */
+		/*
+		user = new User();
+		role = new Role();
+		manuf_scope = new Manuf_scope();
+		order = new Order();
+		product = new Product();
+		contain = new Contain();
+		
 		user.delete(user);
 		role.delete(role);
+		contain.delete(contain);
 		order.delete(order);
 		manuf_scope.delete(manuf_scope);
 		product.delete(product);
+		*/
 	}
 }
