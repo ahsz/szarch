cf2a131364f5391732c191fb264fd6e0cded212f
andras <kandras@digikabel.hu>
2015-11-27 15:26:19 +0100

    Update UserService.java

--- src/com/sample/ejb/UserService.java
+++ src/com/sample/ejb/UserService.java
@@ -2,11 +2,15 @@ package com.sample.ejb;
 
 import java.util.List;
 
-//import com.jpa.entities.Employee;
+
+import com.sample.jpa.entities.User;
+
 
 public interface UserService {
 
-	public void addUser(String emp);
+	public int addUser(User user);
     public String echo(String s);
-	public List getUser();
+	public List<User> getUser(User user);
+	public int updUser(User user);
+	public int remUser(User user);
 }
