c08b7780a0a726fd2f3bd2f513368498a5b86d4c
andras <kandras@digikabel.hu>
2015-11-27 15:26:06 +0100

    Update UserServiceImpl.java

--- src/com/sample/ejb/UserServiceImpl.java
+++ src/com/sample/ejb/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sample.ejb;
 
+import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,43 +9,119 @@ import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolationException;
 import javax.persistence.Query;
+import javax.persistence.TransactionRequiredException;
+import javax.persistence.TypedQuery;
+
+import com.sample.client.MainWindow;
+import com.sample.jpa.entities.User;
+
 
 @Stateless
 @Remote(UserService.class) 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
 	@PersistenceContext(name = "Anyagbeszer")
 	private EntityManager em;
 
+	//private Class<User> entityClass;
+	
 	@Override
-	public void addUser(String emp) {
-
-		em.persist(emp);
-
+	public int addUser(User user) {
+		if ((user.getName() == null || user.getPassword() == null || user.getRole_id() == null) ||  user.getId() != null)
+			return -1;
+		
+		try {
+			em.persist(user);
+		} catch (ConstraintViolationException e) {
+			return -2;
+		} catch (IllegalArgumentException e) {
+			return -3;   
+		} catch (TransactionRequiredException e){
+			return -4;
+		} catch (PersistenceException e) {
+			return -5;
+		}
+			
+		if (em.find(User.class, user.getId()).getName() == user.getName())
+			return em.find(User.class, user.getId()).getId();
+		
+		return 0;
 	}
 	
 	
     @Override
     public String echo(String s) {
  
-        return "Hello "+s;
+        return "Hellooooo44 "+s;
     }
  
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public List getUser() {
+	public List<User> getUser(User user) {
 
-
-
-List x=em.createQuery("select id, name, password, role_id from User ").getResultList();
-			return x;//56d
-
+		@SuppressWarnings("unchecked")
+		TypedQuery<User> query = em.createQuery("from User", User.class);
+		List<User> usr_list = query.getResultList();
 		
-
-
+		return usr_list;	
 	}
 	
+	@Override
+	public int updUser(User user){
+		
+		if ((user.getName() == null && user.getPassword() == null && user.getRole_id() == null) || user.getId() == null )
+			return -1;
+		
+		try {
+			em.merge(user);
+		} catch (ConstraintViolationException e) {
+			return -2;
+		} catch (IllegalArgumentException e) {
+			return -3;   
+		} catch (TransactionRequiredException e){
+			return -4;
+		} catch (PersistenceException e) {
+			return -5;
+		}
+		if (em.find(User.class, user.getId()).getName() == user.getName())
+			return em.find(User.class, user.getId()).getId();
+		
+		return 0;
+	}
+	
+	@Override
+	public int remUser(User user){
+		
+		if ((user.getName() == null || user.getPassword() == null || user.getRole_id() == null) || user.getId() == null )
+			return -1;
 
+
+		try {	
+			em.remove(em.contains(user) ? user : em.merge(user));
+		} catch (ConstraintViolationException e) {
+			return -2;
+		} catch (IllegalArgumentException e) {
+			return -3;   
+		} catch (TransactionRequiredException e){
+			return -4;
+		} catch (PersistenceException e) {
+			return -5;
+		}
+
+		if (em.contains(user))
+			return 0;
+		else
+			return 1;
+	}
+	
 }
